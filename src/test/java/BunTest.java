fcd3a72318230718a4c0c1f9111ca1439fdfd9b8
MAMAYKINA\maria <MaryMamaykina@yandex.ru>
2024-03-02 17:42:58 +0300

    Added generator data for name and price

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -2,10 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import praktikum.Bun;
 import org.apache.commons.lang3.RandomStringUtils;
+import praktikum.generate.data.GenerateName;
+import praktikum.generate.data.GeneratePrice;
 
 public class BunTest {
-    String name = RandomStringUtils.randomAlphabetic(7);
-    Float price = Float.valueOf(RandomStringUtils.randomNumeric(3));
+    String name = new GenerateName().getRandomName();
+    Float price = new GeneratePrice().getRandomPrice();
     Bun bun = new Bun(name, price);
 
     @Test
--- src/test/java/IngredientTest.java
+++ src/test/java/IngredientTest.java
@@ -3,11 +3,13 @@ import org.junit.Assert;
 import org.junit.Test;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
+import praktikum.generate.data.GenerateName;
+import praktikum.generate.data.GeneratePrice;
 
 public class IngredientTest {
     public IngredientType type = IngredientType.FILLING;
-    public String name = RandomStringUtils.randomAlphabetic(7);
-    public float price = Float.valueOf(RandomStringUtils.randomNumeric(3));
+    public String name = new GenerateName().getRandomName();
+    public float price = new GeneratePrice().getRandomPrice();
 
     Ingredient ingredient = new Ingredient(type, name, price);
 
